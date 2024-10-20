e3d4246a0aca78dba6e03d7b48942f7d1ddf3881
yasmeencardenas <123997412+yasmeencardenas@users.noreply.github.com>
2024-10-20 23:55:49 +0300

    Added API

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -6,9 +6,16 @@ import "./Dictionary.css";
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
 
+  function handleResponse(response) {
+    console.log(response.data[0]);
+  }
+
   function search(event) {
     event.preventDefault();
     alert(`Searching for ${keyword} definition`);
+
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word={word}&key={key}`;
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function handleKeywordChange(event) {
