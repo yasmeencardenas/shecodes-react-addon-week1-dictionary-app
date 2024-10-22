27601d9fb3bc2771e85dabc50971b61ca5117113
yasmeencardenas <123997412+yasmeencardenas@users.noreply.github.com>
2024-10-23 00:41:16 +0300

    Updated Meaning.js

--- src/Meaning.js
+++ src/Meaning.js
@@ -5,7 +5,8 @@ export default function Meaning(props) {
   return (
     <div className="Meaning">
       <h3>{props.meaning.partOfspeech}</h3>
-      <p>{props.meaning.definitions[0].definition}</p> {
+      <p>{props.meaning.definitions[0].definition}</p>
+      {props.meaning.definitions.map(function (definition, index) {
         return (
           <div key={index}>
             <p>
@@ -16,7 +17,7 @@ export default function Meaning(props) {
             </p>
           </div>
         );
-      }
+      })}
     </div>
   );
 }
