6c37ef2b6f23795cdbc1b8439f05b9d9a9bc5c1b
yasmeencardenas <123997412+yasmeencardenas@users.noreply.github.com>
2024-10-22 21:33:56 +0300

    Added shecodes logo

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <header className="App-header">
-          <img src={logo} className="App-logo img-fluid" alt="logo" />
-        </header>
         <main>
+          <img src="./shecodeslogo.png"></img>
           <Dictionary />
         </main>
         <footer className="App-footer">
